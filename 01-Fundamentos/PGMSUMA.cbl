f49814bc8a940229dd345e0d16306863138b41f0
m4rtin04 <90196825+m4rtin04@users.noreply.github.com>
2025-08-11 12:39:50 -0300

    Create PGMSUMA.cbl

---
+++ 01-Fundamentos/PGMSUMA.cbl
@@ -0,0 +1,57 @@
+  IDENTIFICATION DIVISION.         
+  PROGRAM-ID. PGMSUMA.             
+ **********************************
+                                   
+  ENVIRONMENT DIVISION.            
+  CONFIGURATION SECTION.           
+  SPECIAL-NAMES.                   
+      DECIMAL-POINT IS COMMA.      
+                                   
+ **********************************
+  DATA DIVISION.                   
+  WORKING-STORAGE SECTION.         
+                                   
+ **********************************
+ *           CONTADORES           *
+ **********************************
+  01 WS-CONTADOR PIC 9(02) VALUE 1.
+ **********************************
+ *           ACUMULADORES         *
+ **********************************
+  01 WS-ACUMULADOR PIC 9(02).      
+                                   
+  PROCEDURE DIVISION.              
+
+  MAIN-PROGRAM.                                   
+                                                  
+      PERFORM 1000-I-INICIO THRU 1000-F-INICIO    
+                                                  
+      PERFORM 2000-I-PROCESO THRU 2000-F-PROCESO  
+                             UNTIL WS-CONTADOR > 10
+                                                  
+      PERFORM 9999-I-FINAL THRU 9999-F-FINAL      
+                                                  
+      GOBACK                                      
+      .                                           
+  F-MAIN-PROGRAM.                                 
+                                                  
+  1000-I-INICIO.                                  
+                                                  
+      INITIALIZE WS-ACUMULADOR                    
+      .                                           
+  1000-F-INICIO. EXIT.                            
+                                                  
+  2000-I-PROCESO.                                 
+                                                  
+      ADD WS-CONTADOR TO WS-ACUMULADOR            
+      ADD 1 TO WS-CONTADOR                        
+      .                                           
+  2000-F-PROCESO. EXIT.  
+
+  9999-I-FINAL.                                   
+                                                 
+     DISPLAY 'LA SUMA DE LOS 10 PRIMEROS NUMEROS'
+     DISPLAY 'RESULTADO: ' WS-ACUMULADOR         
+     .  
+  9999-F-FINAL. EXIT.   
+  
